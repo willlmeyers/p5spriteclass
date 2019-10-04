e38338e23219ea3f4220a4bc550ce76d09c955a9
Matt14c <45790666+Matt14c@users.noreply.github.com>
2019-10-04 09:55:07 -0400

    Update sprite.js

--- sprite.js
+++ sprite.js
@@ -1,12 +1,13 @@
 /*global Sprite,abs,angleMode,append,background,beginShape,bezier,box,camera,ceil,CENTER,color,cone,cos,createCanvas,createCanvas,createGraphics,curveVertex,cylinder,DEGREES,displayHeight,displayWidth,dist,div,DOWN_ARROW,ellipse,endShape,fill,floor,frameCount,frameRate,height,image,key,keyCode,keyIsDown,keyIsPressed,keyIsPressed,keyPressed,LEFT,LEFT_ARROW,lerpColor,line,loadImage,loadJSON,loadSound,map,mouseIsPressed,mouseX,mouseY,noFill,noLoop,normalMaterial,noStroke,p5,plane,point,pointLight,pop,push,push,RADIANS,radians,random,rect,resizeCanvas,resizeCanvas,RIGHT,RIGHT_ARROW,rotate,rotateX,rotateY,rotateZ,round,round,scale,shuffle,sin,sphere,stroke,strokeWeight,text,textAlign,textFont,textSize,texture,textWidth,torus,translate,triangle,UP_ARROW,WEBGL,width,windowHeight,windowHeight,windowWidth,world */
 
 class Sprite{
-  constructor(locationY, locationX, imageArray ){
+  constructor(xSpeed, ySpeed, locationY, locationX, imageArray ){
     this.frames = imageArray;
     this.currentFrame = 0;
     this.x = locationX;
     this.y = locationY;
-    this.velocityy = 100;
+    this.velocityY = ySpeed;
+    this.velocityX = xSpeed;
   }
   
 }
